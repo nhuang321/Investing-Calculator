4a38cc9efd1d08cffe975123e09453e8402b63b0
nhuang321 <nhuang321@gmail.com>
2019-06-23 17:54:15 -0700

    Updates

---
+++ precache-manifest.180c95c49d07f810cfe853fbf9eef344.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc37b735af1b0d3d74b22d66ce24e64",
+    "url": "/investing-calculator/index.html"
+  },
+  {
+    "revision": "4d41e04d9f98f6401da7",
+    "url": "/investing-calculator/static/css/main.5a5330bf.chunk.css"
+  },
+  {
+    "revision": "f7c8cd44e48bdb317efb",
+    "url": "/investing-calculator/static/js/2.e8afb4b0.chunk.js"
+  },
+  {
+    "revision": "4d41e04d9f98f6401da7",
+    "url": "/investing-calculator/static/js/main.ec71fc6b.chunk.js"
+  },
+  {
+    "revision": "a129d3b1349da206ba52",
+    "url": "/investing-calculator/static/js/runtime~main.1fc4e9f0.js"
+  },
+  {
+    "revision": "082140cf88553391dc7be4dc25080086",
+    "url": "/investing-calculator/static/media/airpods.082140cf.jpg"
+  },
+  {
+    "revision": "a43bcc25543f3d9683bd7bde83216e97",
+    "url": "/investing-calculator/static/media/chipotle.a43bcc25.jpg"
+  },
+  {
+    "revision": "c55a7ba94347ff2d8d9aec74ef16c7a9",
+    "url": "/investing-calculator/static/media/college.c55a7ba9.jpg"
+  },
+  {
+    "revision": "5b9f44abf002edb0e2ab427cad6e14e9",
+    "url": "/investing-calculator/static/media/hamilton.5b9f44ab.jpg"
+  },
+  {
+    "revision": "60f75dfdfed4dffe429eada1ec79441c",
+    "url": "/investing-calculator/static/media/money.60f75dfd.png"
+  },
+  {
+    "revision": "52fe924eb226af36048232a62ab5195c",
+    "url": "/investing-calculator/static/media/nyapartment.52fe924e.jpg"
+  },
+  {
+    "revision": "a78fc3c77bbaa06fae1ddfd44d88716b",
+    "url": "/investing-calculator/static/media/roadTrip.a78fc3c7.jpg"
+  },
+  {
+    "revision": "5ef4ca702c0a38c19a5ec3f0fd85271e",
+    "url": "/investing-calculator/static/media/tesla.5ef4ca70.jpg"
+  },
+  {
+    "revision": "6110f0def597ece7b2adfd10d6d15243",
+    "url": "/investing-calculator/static/media/vacation.6110f0de.jpg"
+  },
+  {
+    "revision": "8240634bc5b44b285fa4de4688b69492",
+    "url": "/investing-calculator/static/media/yacht.8240634b.jpg"
+  }
+]);
